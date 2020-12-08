338689db435e884ec83dfde8ff7df24d8aa01453
Didier Bröska <didier.beheydt@gmail.com>
2020-12-08 14:58:11 +0100

    base of cli entrey point

---
+++ consistent_depth/cli/cli.py
@@ -0,0 +1,16 @@
+"""
+Main cli app.
+"""
+
+import sys
+
+# from .params import AppParams
+
+
+def main():
+    pass
+    # params = AppParams()
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
